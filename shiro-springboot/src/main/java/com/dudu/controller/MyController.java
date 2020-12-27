61c735f1205d8c271570b63af043c3e7c1085a07
944969864 <75293645+944969864@users.noreply.github.com>
2020-12-27 17:11:47 +0800

    Update MyController.java
    认证 登录

--- shiro-springboot/src/main/java/com/dudu/controller/MyController.java
+++ shiro-springboot/src/main/java/com/dudu/controller/MyController.java
@@ -29,4 +29,22 @@ public class MyController {
     public String toLogin(){
         return "login";
     }
+     @RequestMapping("/login")
+    public String login(String username,String password,Model model){
+        //获取当前用户
+        Subject subject = SecurityUtils.getSubject();
+        //封装用户的登录数据
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        try {
+            subject.login(token);//执行登录方法，如果没有异常就ok了
+            return "index";
+        } catch (UnknownAccountException e) {//用户名不存在
+            model.addAttribute("msg","用户名错误");
+            return "login";
+        }catch (IncorrectCredentialsException e) {//密码不存在
+            model.addAttribute("msg","密码错误");
+            return "login";
+        }
+
+    }
 }
